ec8f3cc7a428f7a2a1cf3f711b0f4a5aeb994696
marccodera <marc.codera@gmail.com>
2018-07-17 19:21:16 +0200

    Map Soom modified

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.5621635, lng: 2.0127137},
-        zoom: 13
+        zoom: 15
     });
     var terrassa = {lat: 41.564432, lng: 2.010869};
     var marker = new google.maps.Marker({
