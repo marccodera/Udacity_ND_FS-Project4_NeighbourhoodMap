098306a5a1ce7439cb0d9d36e0c819cac447102a
marccodera <marc.codera@gmail.com>
2018-07-17 19:17:16 +0200

    Added map bounds and first marker

--- js/app.js
+++ js/app.js
@@ -14,8 +14,14 @@ var map;
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
+        center: {lat: 41.5621635, lng: 2.0127137},
         zoom: 13
+    });
+    var terrassa = {lat: 41.564432, lng: 2.010869};
+    var marker = new google.maps.Marker({
+        position: terrassa,
+        map: map,
+        title: 'First Marker!'
     });
 }
 
