3986dbc032134d15a47725cfc6bf5cbd6d59f252
marccodera <marc.codera@gmail.com>
2018-07-30 17:24:22 +0200

    Index issue corrected and maps styles generated, added foursquare picture, but 50 API calls permitted per day though

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 /** Model **/
 var initialLocations = [
-    {title: "Minyons de Terrassa", category: "Local Culture", location: {lat : 41.564611,lng : 2.012825}, address: "Carrer del Teatre, 4-6, 08221 Terrassa, Barcelona", IgLocationId: 7857882},
-    {title: "Biblioteca Central", category: "Library", location: {lat: 41.569068, lng: 2.009941}, address: "Passeig de les Lletres, 1, 08221 Terrassa, Barcelona", IgLocationId: 279763804},
-    {title: "Hospital Mutua de Terrassa", category: "Hospital", location: {lat: 41.564225, lng: 2.0143027}, address: "Plaça del Doctor Robert, 5, 08221 Terrassa, Barcelona", IgLocationId: 1692770984311166},
-    {title: "MNACTec", category: "Museum", location: {lat: 41.565395, lng: 2.007131}, address: "Rambla d'Ègara, 270, 08221 Terrassa, Barcelona", IgLocationId: 769808843179516},
-    {title: "RENFE Estació del Nord", category: "Transport", location: {lat: 41.569748, lng: 2.014265}, address: "Plaça Estació del Nord, 08221 Terrassa, Barcelona", IgLocationId: 239465053074271},
-    {title: "Anytime Fitness", category: "Gym", location: {lat: 41.570042, lng: 2.004469}, address: "Avinguda de Josep Tarradellas, 2, 08225 Terrassa, Barcelona", IgLocationId: 1024420731},
-    {title: "Qsport", category: "Gym", location: {lat: 41.5688617, lng: 2.0067464}, address: "Carrer de Sant Gaietà, 107, 08221 Terrassa, Barcelona", IgLocationId: 468202}
+    {title: "Minyons de Terrassa", category: "Local Culture", location: {lat : 41.564611,lng : 2.012825}, address: "Carrer del Teatre, 4-6, 08221 Terrassa, Barcelona", FSVenueId: "4ce5a6f6595cb1f7f593b714"},
+    {title: "Biblioteca Central", category: "Library", location: {lat: 41.569068, lng: 2.009941}, address: "Passeig de les Lletres, 1, 08221 Terrassa, Barcelona", FSVenueId: "4bbf634b98f49521cc90d263"},
+    {title: "FGC Rambla", category: "Transport", location: {lat: 41.560306, lng: 2.007727}, address: "Plaça de Parc dels Catalans, 98, 08221 Terrassa, Barcelona", FSVenueId: "4be8f711ee96c9283db9febf"},
+    {title: "MNACTec", category: "Museum", location: {lat: 41.565395, lng: 2.007131}, address: "Rambla d'Ègara, 270, 08221 Terrassa, Barcelona", FSVenueId: "50c32304e4b0309a63ec38e5"},
+    {title: "RENFE Estació del Nord", category: "Transport", location: {lat: 41.569748, lng: 2.014265}, address: "Plaça Estació del Nord, 08221 Terrassa, Barcelona", FSVenueId: "4b6bcd3cf964a520fc182ce3"},
+    {title: "Anytime Fitness", category: "Gym", location: {lat: 41.570042, lng: 2.004469}, address: "Avinguda de Josep Tarradellas, 2, 08225 Terrassa, Barcelona", FSVenueId: "568edd57498ee9de014b6a1b"},
+    {title: "Qsport", category: "Gym", location: {lat: 41.5688617, lng: 2.0067464}, address: "Carrer de Sant Gaietà, 107, 08221 Terrassa, Barcelona", FSVenueId: "4bb2357a715eef3bf0e984bb"}
 ]
 
 var initialCategories = [
     {id: 1, title: "Local Culture"},
     {id: 2, title: "Library"},
-    {id: 3, title: "Hospital"},
-    {id: 4, title: "Museum"},
-    {id: 5, title: "Transport"},
-    {id: 6, title: "Gym"},
+    {id: 3, title: "Museum"},
+    {id: 4, title: "Transport"},
+    {id: 5, title: "Gym"}
 ]
 
 
@@ -26,7 +25,7 @@ var Location = function(data) {
     this.lng = data.location.lng;
     this.address = data.address;
     this.marker = data.location.marker;
-    this.IgLocationId = data.IgLocationId;
+    this.FSVenueId = data.FSVenueId;
 }
 
 
@@ -93,10 +92,86 @@ function initMap() {
     // variable declarating where the center of the map will be and for use with markers
     var terrassa = {lat: 41.564432, lng: 2.010869};
 
+    // variable declaring map style
+    var styles = [
+        {
+           "featureType": "landscape.natural",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#e0efef"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "hue": "#1900ff"
+                },
+                {
+                    "color": "#c0e8e8"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 100
+                },
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "lightness": 700
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+
+                    "color": "#7dcdcd"
+                }
+            ]
+
+        }
+    ];
+
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: terrassa,
         zoom: 15,
+        styles: styles,
         mapTypeControl: false
     });
 
@@ -105,29 +180,39 @@ function initMap() {
 
     // Creating a Marker for each location in the model
     for(var i = 0; i < initialLocations.length; i++) {
-        // Instagram API Key: 9e7d6563f18a495ab49a6b8b5d255069
-        // instagram GET URL: https://api.instagram.com/v1/locations/7857882?access_token=9e7d6563f18a495ab49a6b8b5d255069
+        $.ajax({
+            url: "https://api.foursquare.com/v2/venues/" + initialLocations[i].FSVenueId + "/?client_id=F0MVBVM3VWYUUU1E2I1NHV2C4JXPSD5Y3ALTR2FFVTK04O2B&client_secret=VGBYLZYS12FN5UQ0JGIJKFDYSI14QDZ3I5B5F2CTHCYDAA2O&v=20180724",
 
-        // Creating marker with information from the model and from the instagram API
-        var marker = new google.maps.Marker({
-            id: i,
-            address: initialLocations[i].address,
-            position: initialLocations[i].location,
-            category: initialLocations[i].category,
-            title: initialLocations[i].title,
-            map: map,
-            animation: google.maps.Animation.DROP
-        });
+            //url: "https://api.foursquare.com/v2/venues/search?ll=" + initialLocations[i].location.lat + "," + initialLocations[i].location.long + "&near=Terrassa, Barcelona&client_id=F0MVBVM3VWYUUU1E2I1NHV2C4JXPSD5Y3ALTR2FFVTK04O2B&client_secret=VGBYLZYS12FN5UQ0JGIJKFDYSI14QDZ3I5B5F2CTHCYDAA2O&v=20180205&query=pizza",
+            dataType: "jsonp",
+            success: (function(i, data){
+                initialLocations[i].description = "<img src=" + data.response.venue.bestPhoto.prefix + "200x200" + data.response.venue.bestPhoto.suffix + ">";
+                //initialLocations[i].description = data.response.venues.name;
 
-        // Push the created marker to the markers array
-        markers.push(marker);
+                // Creating marker with information from the model and from the instagram API
+                var marker = new google.maps.Marker({
+                    id: i,
+                    address: initialLocations[i].address,
+                    position: initialLocations[i].location,
+                    category: initialLocations[i].category,
+                    title: initialLocations[i].title,
+                    description: initialLocations[i].description,
+                    index: i,
+                    map: map,
+                    animation: google.maps.Animation.DROP
+                });
 
-        // Adding marker as a property inside each location in the model
-        initialLocations[i].index = i;
+                // Push the created marker to the markers array
+                markers.push(marker);
 
-        // Create an onClick event for each marker
-        marker.addListener('click', function() {
-            populateInfoWindow(this, infoWindow);
+                // Adding marker as a property inside each location in the model
+                initialLocations[i].index = i;
+
+                // Create an onClick event for each marker
+                marker.addListener('click', function() {
+                    populateInfoWindow(this, infoWindow);
+                });
+            }).bind(null, i)
         });
     }
 }
@@ -139,7 +224,7 @@ function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<h3>' + marker.title + '</h3>'+ '<div>' + marker.category + '</div>' + '<div>' + marker.address + '</div>');
+        infowindow.setContent('<h3>' + marker.title + '</h3>'+ '<div>' + marker.category + '</div>' + '<div>' + marker.address + '</div>' + marker.description);
         infowindow.open(map, marker);
         // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick', function() {
